a06a28517a9c3695ac1f9a6bdcf3ce2a433cdc58
syamimi-aziah <syamimi.molpay@gmail.com>
2017-03-31 11:01:25 +0800

    Update process_callback.php
    Record transaction changes history in order status history.

--- process_callback.php
+++ process_callback.php
@@ -13,7 +13,7 @@ $info = ( $HTTP_POST_VARS )?$HTTP_POST_VARS:$_POST;
 
 $nbcb 	  = $_POST['nbcb'];
 
-$domain	  = MODULE_PAYMENT_MOLPAY_ID;
+$domain	  = $info['domain'];
 $amount   = $info['amount'];
 $orderid  = $info['orderid'];
 $tranID   = $info['tranID'];
@@ -25,35 +25,60 @@ $channel  = $info['channel'];
 $skey     = $info['skey'];
 $password = MODULE_PAYMENT_MOLPAY_VKEY;
 
+$excluded = ['domain', 'skey', 'nbcb', 'treq', 'extraP'];
+$comment = "Payment Info\n\r";
+
+foreach($info as $k => $v){
+    if(!in_array($k, $excluded))
+        $comment .= $k." :".$v.", ";
+}
+
+if($status == "11")
+  $sr = "Failed";
+elseif($status == "22")
+  $sr = "Pending";
+elseif($status == "00")
+  $sr = "Success";
+
+$comment .= "Status Remark :".$sr;
+
 $key0 = md5($tranID.$orderid.$status.$domain.$amount.$currency);
 $key1 = md5($paydate.$domain.$key0.$appcode.$password);
 
-if($skey==$key1)
+if($skey != $key1)
 {
-	if ($status=="00") 
-	{
-		$db->Execute("update " . TABLE_ORDERS . "
-							set orders_status = 2
-	                        where orders_id = '" . (int)$orderid . "'");
-	}
-	elseif($status=="11")
-	{
-		$db->Execute("update " . TABLE_ORDERS . "
-							set orders_status = 1
-	                        where orders_id = '" . (int)$orderid . "'");
-	}
-	elseif($status=="22")
-	{
-		$db->Execute("update " . TABLE_ORDERS . "
-							set orders_status = 1
-	                        where orders_id = '" . (int)$orderid . "'");
-	}
+     exit();    
+}
+if ($status=="00")
+{
+    $db->Execute("update " . TABLE_ORDERS . "
+    set orders_status = 2 where orders_id = '" . (int)$orderid . "'");
+
+    $sql_data_array = array('orders_id' => (int)$orderid,
+                            'orders_status_id' => 2,
+                            'date_added' => 'now()',
+                            'customer_notified' => false,
+                            'comments' => $comment
+                           );
+
+    zen_db_perform(TABLE_ORDERS_STATUS_HISTORY, $sql_data_array);
+
+}
+elseif($status=="11")
+{
+                $db->Execute("update " . TABLE_ORDERS_STATUS_HISTORY . "
+                              set comments ='" .$comment."' where orders_id = '" . (int)$orderid . "'");
+}
+elseif($status=="22")
+{
+                $db->Execute("update " . TABLE_ORDERS_STATUS_HISTORY . "
+                              set comments ='" .$comment."' where orders_id = '" . (int)$orderid . "'");
 }
 
 if($nbcb==1)
 {
-	echo 'CBTOKEN:MPSTATOK​';
-	exit();
+        echo 'CBTOKEN:MPSTATOK';
+        exit();
 }
 
 ?>
